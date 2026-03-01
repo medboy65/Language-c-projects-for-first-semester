bd5004755eee5a1def95d4689cc1f03f0848ed23
medboy65 <153338098+medboy65@users.noreply.github.com>
2026-03-01 08:53:09 +0800

    Update Exercice 2.c

--- Exercice 2.c
+++ Exercice 2.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-    // Set the letters that spell out "China" in separate variables
+    // Set the letters that spell out "China" in separate variables                            //@Mohammed Lamkhayar 24070260026//
     // Each character is handled individually for flexibility
-    char c1 = 'C', c2 = 'h', c3 = 'i', c4 = 'n', c5 = 'a';
+    char c1 = 'C', c2 = 'h', c3 = 'i', c4 = 'n', c5 = 'a';                                                            //@Mohammed Lamkhayar 24070260026//
 
     // Adjust each character by shifting it by 4 places
     // This operation modifies each letter to a different one by adding a constant value
@@ -12,12 +12,13 @@ int main()
     c2 = c2 + 4; // 'h' becomes 'l'
     c3 = c3 + 4; // 'i' becomes 'm'
     c4 = c4 + 4; // 'n' becomes 'r'
-    c5 = c5 + 4; // 'a' becomes 'e'
+    c5 = c5 + 4; // 'a' becomes 'e'                                                                    //@Mohammed Lamkhayar 24070260026//            //@Mohammed Lamkhayar 24070260026//
 
     // Output the new set of characters as one string
     // The letters are printed together as a new word, reflecting the changes
     printf("%c%c%c%c%c\n", c1, c2, c3, c4, c5);
 
-    // End the program with a successful exit
+    // End the program with a successful exit                                                    //@Mohammed Lamkhayar 24070260026//
     return 0;
 }
+
