c1b91af19c7dce57b4f6e7289c3bc2d876085421
medboy65 <153338098+medboy65@users.noreply.github.com>
2026-03-01 08:53:46 +0800

    Refactor grading program and add comments

--- Exercice 3.c
+++ Exercice 3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-int main()
+int main()                                          //@Mohammed Lamkhayar 24070260026//
 {
  float grade;
     do{
-    printf("Enter a value within 0 to 100:");
+    printf("Enter a value within 0 to 100:");                              //@Mohammed Lamkhayar 24070260026//
     scanf("%f",&grade);
     if(grade>=0 && grade<=100){
     if(grade>=90 && grade<=100){
         printf("A");
     }
-     if(grade>=80 && grade<=89){
+     if(grade>=80 && grade<=89){   //@Mohammed Lamkhayar 24070260026//
         printf("B");
     }
      if(grade>=70 && grade<=79){
@@ -18,7 +18,7 @@ int main()
      if(grade>=60 && grade<=69){
         printf("D");
     }
-     if(grade>=0 && grade<60){
+     if(grade>=0 && grade<60){                                                                    //@Mohammed Lamkhayar 24070260026//            //@Mohammed Lamkhayar 24070260026//
         printf("E");
     }
     }else{
@@ -27,3 +27,4 @@ int main()
     }while(grade<0 || grade>100);
        return 0;
 }
+//@Mohammed Lamkhayar 24070260026//
